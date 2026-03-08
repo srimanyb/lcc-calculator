2dd10cec1ab47c46e92678e8972fe769a7f4a380
Sriman <srimanyb10@gmail.com>
2026-03-08 11:01:16 +0530

    final fix db connection

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const path = require('path');
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 const app = express();
 
